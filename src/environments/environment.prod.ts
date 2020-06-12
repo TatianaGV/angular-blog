ed3014f3e255d5d8ceaebb664ecdfd887890b281
TatianaGV <tanyanahaus@yandex.ru>
2020-06-12 23:51:24 +0300

    added work with firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
-export const environment = {
-  production: true
+import {Environment} from "./interface";
+
+export const environment : Environment = {
+  production: true,
+  apiKey: 'AIzaSyBO4kvhxVex2suq5bhaymj67D5xycNNqxE'
 };
